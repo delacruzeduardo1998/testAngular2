b81784bec35f28814a20ff2c1e094100a8a3de09
delacruzeduardo1998 <delacruzeduard98@gmail.com>
2020-10-18 22:01:13 -0500

    deploying 2

--- server.js
+++ server.js
@@ -5,3 +5,4 @@ app.use(express.static(__dirname + '/dist/test-angular2'));
 app-get('/*', function(req,res){
 res.sendFile(path.join(__dirname + '/dist/test-angular2/index.html'));});
 app.listen(process.env.PORT || 8080);
+
